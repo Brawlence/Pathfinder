6a63b0926711d16fb0bc0ee68386eb64fd201b4e
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-02-02 07:03:02 +0300

    Save the state!
    LocalStorage to keep track of contrast and nightmode

--- prereq/main_script.js
+++ prereq/main_script.js
@@ -1,12 +1,7 @@
 "use strict";
 
-var r = document.getElementById("rbw"),
-    currentHue = 0,
-    hueAddition = 5,
-    rainbowTiming = 1000 / 25,
-    documentElement = document.getElementsByTagName("html")[0],
-    clickEvent = "ontouchstart" in window ? "touchend" : "click",
-    classMethods = ["remove", "add"],    
+var clickEvent = "ontouchstart" in window ? "touchend" : "click",
+    classMethods = ["remove", "add"],
     stringArray = [ "Добавить контраста",
                     "Убрать контраст",
                     "Ночной режим",
@@ -27,26 +22,17 @@ function createControls() {
 function someControl(id, textArr, className) {
     var el = document.getElementsByTagName("html")[0];
     var acbox = document.getElementById(id),
-        textNode = acbox.firstChild,
-        toggled = false;
+        textNode = acbox.firstChild;
     acbox.addEventListener(
         clickEvent,
         function() {
-            var selector = Number((toggled = !toggled));
+            var selector = Number(localStorage.getItem(id)!=='true');
+            localStorage.setItem(id, Boolean(selector));
             textNode.data = textArr[selector];
             el.classList[classMethods[selector]](className);
         },
         false
     );
-}
-
-// Rainbow shifting text
-function doThatFuckingColorThing() {
-    var color = "hsl(" + currentHue + ", 80%, 60%)",
-        nextHue = currentHue + hueAddition;
-    currentHue = nextHue > 360 ? 0 : nextHue;
-    r.style.color = color;
-    setTimeout(doThatFuckingColorThing, rainbowTiming);
 }
 
 function addContrastControl() {
@@ -58,7 +44,13 @@ function addInvertedControl() {
 }
 
 createControls();
-
+if (localStorage.getItem('invmode')==='true') {
+    document.getElementById('invmode').firstChild.data = stringArray[3];
+    document.getElementsByTagName("html")[0].classList.add("inverted");
+}
+if (localStorage.getItem('contrast')==='true') {
+    document.getElementById('contrast').firstChild.data = stringArray[1];
+    document.getElementsByTagName("html")[0].classList.add("contrast");
+}
 addContrastControl();
 addInvertedControl();
-doThatFuckingColorThing();
