7f3d2925a225033cfbaab754123a03c9e8742dc8
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-03-15 00:50:57 +0300

    Сделал генератор гистограмм на главной

--- prereq/histogram.js
+++ prereq/histogram.js
@@ -1,7 +1,11 @@
-const ROLLS = 10000;
-const CHARS = 10000;
+const ROLLS = 100000;
+const CHARS = 100000;
+const SCALE = 200;
+const modi = [-5,-5,-4,-4,-3,-3,-2,-2,-1,-1,0,1,1,2,2,3, 3, 4, 4];
+const cost = [-4,-4,-4,-4,-4,-4,-4,-4,-2,-1,0,1,2,3,5,7,10,13,17];
 
 function maxThreeFromFour() {
+    dice = [0,0,0,0];
     for (i=0;i<4;i++) {
         dice[i] = Math.floor(Math.random() * 6.0) + 1;
     };
@@ -14,75 +18,79 @@ function printHisto(histogram) {
     for (v=0;v < histogram.length; v++) {
         maxVal = Math.max(maxVal,histogram[v]);
     };
-    console.log("Stat distribution histogram:");
     for (v=0;v < histogram.length; v++) {
         tempstr = "";
-        spacer1 = "";
+        spacer1 = " ";
         spacer2 = "";
-        if (v < 10) spacer1 = " ";
-        histogram_corr = histogram[v] * 100 / maxVal;
+        if (v < 10) spacer1 = "  ";
+        histogram_corr = histogram[v] * SCALE / maxVal;
         histogram_pres = (1.0 * histogram[v])/ROLLS;
         switch (histogram_pres.toString().length) {
             case 1:
-                spacer2 = "     ";
+                spacer2 = "       ";
                 break;
             case 2:
-                spacer2 = "    ";
+                spacer2 = "      ";
                 break;
             case 3:
-                spacer2 = "   ";
+                spacer2 = "     ";
                 break;
             case 4:
-                spacer2 = "  ";
+                spacer2 = "    ";
                 break;
             case 5:
-                spacer2 = " ";
+                spacer2 = "   ";
                 break;
             case 6:
-                spacer2 = "";
+                spacer2 = "  ";
+                break;
+            case 7:
+                spacer2 = " ";
                 break;
             default:
-                spacer2 = "";
+                spacer2 = " ";
                 break;
         };
         for (k=0;k<histogram_corr;k++) {
             tempstr = tempstr + "*";
         };
-        console.log(v + spacer1 + " " + histogram_pres + spacer2 +  " " + tempstr);
+        if (histogram_pres > 0) console.log(v + spacer1 + histogram_pres + spacer2 + tempstr);
     }
 }
 
 function histo(){
     histogram = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    cost_histo = [];
-    cost_histo = cost_histo.fill(0);
-    diceSum = 0;
-    dice = [0,0,0,0];
-    for (j=0;j<ROLLS;j++) {
-        diceSum = maxThreeFromFour();
-        histogram[diceSum] = histogram[diceSum]+1;
+    costHistogram = [];
+        for (i = 0; i < 111; i++) {
+            costHistogram[i] = 0;
+        }
+    for (j = 0; j < ROLLS; j++) {
+        histogram[maxThreeFromFour()]++;
     };
+    console.log("Stat distribution histogram:");
     printHisto(histogram);
 
-    modi = [-5,-5,-4,-4,-3,-3,-2,-2,-1,-1,0,1,1,2,2,3,3,4,4]
-    cost = [-4,-4,-4,-4,-4,-4,-4,-4,-2,-1,0,1,2,3,5,7,10,13,17];
     // если все характеристики =< 13, либо сумма модификаторов =< +3 - переброс
-    
-    for (i=0;i<CHARS;i++) {
+   
+    for (k = 0; k < CHARS; k++) {
         tempChara = [10,10,10,10,10,10];
-        validMods = false;
         validStats = false;
+        validMods = false;
         modiSum = 0;
         costSum = 0;
-        for (j=0;j<6;j++) {
-            tempChara[j] = maxThreeFromFour();
+        for (l = 0; l < tempChara.length; l++) {
+            tempChara[l] = maxThreeFromFour();
+            if (tempChara[l] > 13) validStats = true;
+            modiSum = modiSum + modi[tempChara[l]];
         };
-        for (k=0;k<tempChara.length;k++) {
-             if (tempChara[k] > 13) validStats = true;
-             modiSum = 
+        if (modiSum > 3) validMods = true; 
+        if ((validStats == true) && (validMods == true)) {
+            for (m = 0; m < tempChara.length; m++) {
+                costSum = costSum + cost[tempChara[m]];
+            };
+            costHistogram[costSum]++;
         };
-
-        // valid by Modi?
-
     };
+    console.log("Cost distribution histogram:");
+    printHisto(costHistogram);
 }
